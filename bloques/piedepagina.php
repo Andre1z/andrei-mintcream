67dfab81ae4c4eb6d4f807969b30aa1a350ca4c2
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2025-05-12 15:28:57 +0200

    Update piedepagina.php

---
+++ bloques/piedepagina.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * piedepagina.php
+ *
+ * Genera el pie de página de la aplicación, mostrando un simple mensaje de copyright
+ * y cierra las etiquetas abiertas (<main>, <body> y <html>).
+ *
+ * @package Mintcream
+ */
+?>
+</main>
+<footer>
+    <p>Andrei | Mintcream &copy; <?php echo date("Y"); ?></p>
+</footer>
+</body>
+</html>
